72dcbdfd9ece1936fee794fc4bccab664ca53ac5
Juri Pakaste <juri@iki.fi>
2016-06-17 09:44:57 +0300

    Read required settings values earlier, raise ValueError if no tenant found

--- tenant_filter/models.py
+++ tenant_filter/models.py
@@ -5,7 +5,8 @@ from django.conf import settings
 
 
 TENANT_FILTER = getattr(settings, 'TENANT_FILTER')
-
+TENANT_USER_OBJ_NAME = TENANT_FILTER['TENANT_USER_MODEL'].split('.')[-1].lower()
+TENANT_OBJ_NAME = TENANT_FILTER['TENANT_MODEL'].split('.')[-1].lower()
 
 class TenantFilterManager(models.Manager):
     def get_queryset(self):
@@ -16,8 +17,10 @@ class TenantFilterManager(models.Manager):
         qs = super(TenantFilterManager, self).get_queryset()
         user = get_current_user()
         if user and user.is_authenticated():
-            tenant_user_obj = getattr(user, TENANT_FILTER['TENANT_USER_MODEL'].split('.')[-1].lower())
-            tenant_obj = getattr(tenant_user_obj, TENANT_FILTER['TENANT_MODEL'].split('.')[-1].lower())
+            tenant_user_obj = getattr(user, TENANT_USER_OBJ_NAME, None)
+            if tenant_user_obj is None:
+                raise ValueError("No tenant defined for user")
+            tenant_obj = getattr(tenant_user_obj, TENANT_OBJ_NAME)
             filter_dict = {TENANT_FILTER['TENANT_FK_NAME']: tenant_obj.pk}
             qs = qs.filter(**filter_dict)
         return qs
